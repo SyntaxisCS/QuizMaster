f6d2276c62f8dada8ac8e4238b354470f007e298
Syntaxis-Web <cgissell2012@gmail.com>
2021-09-28 23:59:13 -0400

    0.0.2
    Database role specification

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ let bothost = botconfig.host;
 let statuses = botconfig.statuses;
 
 client.on("ready", () => {
-	console.log(`Icebot (${botver}) is online in ${client.guilds.cache.size} servers`);
+	console.log(`QuizMaster (${botver}) is online in ${client.guilds.cache.size} servers`);
 });
 
 client.on("messageCreate", async (msg) => {
   if(!msg.author.bot) {
 	
 	// Config Handling
-	const DB = new Database("DATABASE NAME GOES HERE") // Replace this  	
+	const DB = new Database("QuizMasterDB.sqlite"); // Replace this  	
 	let sql = DB.prepare(`INSERT OR IGNORE INTO GuildConfig (guildId) VALUES (${msg.guild.id});`);
 	sql.run();
 
@@ -50,6 +50,102 @@ client.on("messageCreate", async (msg) => {
 				msg.channel.send(`Pong!(${pingCalc}ms)`);
 			});
 		}
+
+	// Role Handling
+		// Teacher
+			if (cmd === prefix + "addteacher") {
+				// Create teacher with @mention
+			}
+
+			if (cmd === prefix + "deleteteacher") {
+				// Delete by id
+			}
+
+		// Student
+			if (cmd === prefix + "addstudent") {
+				// Create student with @mention
+			}
+
+			if (cmd === prefix + "deletestudent") {
+				// Delete by id
+			}
+
+
+	// Moderation
+
+	if (cmd === prefix + "setprefix") {
+		msg.delete();
+		if (!msg.member.permissions.has("ADMINISTRATOR")) {
+			msg.channel.send(`${author}, that is an admin only command!`);
+		} else {
+			if(!args) {
+				msg.channel.send(`Please specify a new prefix to use! This server's prefix is ${prefix}`);
+			} else {
+				let sql = DB.prepare(`UPDATE GuildConfig SET prefix = '${args}' WHERE guildId = ${msg.guild.id};`);
+				sql.run();
+
+				let prefixQuery = DB.prepare(`SELECT prefix FROM 'GuildConfig' WHERE guildId = ${msg.guild.id}`);
+				prefix = prefixQuery.get().prefix;
+				msg.channel.send(`You have changed the server prefix to ${prefix}`);
+			}
+		}
+	}
+
+	if (cmd === prefix + "admin") {
+		msg.delete();
+		let adminGetQuery = DB.prepare(`SELECT adminRole FROM 'GuildConfig' WHERE guildId = ${msg.guild.id}`);
+		let adminGet = adminGetQuery.get().adminRole;
+		if(!msg.member.permissions.has("ADMINISTRATOR")) {
+			msg.channel.send(`${author}, that is an admin only command!`);
+		} else {
+			if(!args) {
+				msg.channel.send(`The current admin role is ${adminGet}. Set a new one with admin set`);
+			} else {
+				if (args === "set") {
+					let sql = DB.prepare(`UPDATE GuildConfig SET adminRole = '${args2}' WHERE guildId = ${msg.guild.id}`);
+					sql.run();
+				}
+			}
+		}
+	}
+
+	if (cmd === prefix + "teacher") {
+		msg.delete();
+		let teacherGetQuery = DB.prepare(`SELECT teacherRole FROM 'GuildConfig' WHERE guildId = ${msg.guild.id}`);
+		let teacherGet = teacherGetQuery.get().teacherRole;
+		if(!msg.member.permissions.has("ADMINISTRATOR")) {
+			msg.channel.send(`${author}, that is an admin only command!`);
+		} else {
+			if(!args) {
+				msg.channel.send(`The current teacher role is ${teacherGet}. Set a new one with teacher set`);
+			} else {
+				if (args === "set") {
+					let sql = DB.prepare(`UPDATE GuildConfig SET teacherRole = '${args2}' WHERE guildId = ${msg.guild.id}`);
+					sql.run();
+				}
+			}
+		}
+	}
+
+	if (cmd === prefix + "student") {
+		msg.delete();
+		let studentGetQuery = DB.prepare(`SELECT studentRole FROM 'GuildConfig' WHERE guildId = ${msg.guild.id}`);
+		let studentGet = studentGetQuery.get().studentRole;
+		if(!msg.member.permissions.has("ADMINISTRATOR")) {
+			msg.channel.send(`${author}, that is an admin only command!`);
+		} else {
+			if(!args) {
+				msg.channel.send(`The current student role is ${studentGet}. Set a new one with student set`);
+			} else {
+				if (args === "set") {
+					let sql = DB.prepare(`UPDATE GuildConfig SET studentRole = '${args2}' WHERE guildId = ${msg.guild.id}`);
+					sql.run();
+				}
+			}
+		}
+	}
+
+	DB.close();
   }
 });
 
